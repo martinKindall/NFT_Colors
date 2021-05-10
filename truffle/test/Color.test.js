45e8b855f6dd34ca9da8466539d34467085b3883
martinKindall <mart256@gmail.com>
2021-05-10 22:30:58 +0200

    Added test for checking Contract name

--- truffle/test/Color.test.js
+++ truffle/test/Color.test.js
@@ -1,9 +1,19 @@
 const Color = artifacts.require('Color');
 
 contract('Color NFT', ([someUser]) => {
-    it('A color can be created', async() => {
-        const aColor = await Color.new();
+    let aColor;
+
+    beforeEach(async () => {
+        aColor = await Color.new();
+    });
+
+    it('A color can be created', async () => {
         assert.isNotNull(aColor.address);
         assert.isAtLeast(aColor.address.length, 40);
+    });
+
+    it('Has a name', async () => {
+        const name = await aColor.name();
+        assert.equal(name, "Unique Colors");
     });
 });
