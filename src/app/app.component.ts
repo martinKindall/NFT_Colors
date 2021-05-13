75381dc6519b1d2bd5a5f8a143d6c977c2ec02b2
martinKindall <mart256@gmail.com>
2021-05-13 16:01:14 +0200

    Iterating over colors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  colors: string[] | undefined;
 
   constructor() {
   }
